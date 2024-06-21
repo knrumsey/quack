8e728f5db0ef9b3ddab1db12ab8b71bb65fc8487
Kelin Rumsey <knrumsey@pn2004129.lanl.gov>
2024-06-21 00:38:20 -0500

    recursive A generator (much faster)

--- R/chaos.R
+++ R/chaos.R
@@ -37,14 +37,9 @@ bayes_chaos_wrapper <- function(X, y, n, p, md, mo, mu_y, sig_y, realmd, lambda,
     cat("Starting model with max degree = ", md, " and max order = ", mo, "\n", sep="")
   }
   if(verbose) cat("\tComputing initial phi matrix\n")
-  args_list <- replicate(p, 0:md, simplify = FALSE)
-  A_set <- do.call(expand.grid, args_list)[-1,] # remove 0, 0, 0 row
-  A_degree <- apply(A_set, 1, sum)
-  A_order  <- apply(A_set, 1, function(aa) sum(aa > 0))
-  ind <- (A_degree <= md) & (A_order <= mo)
-  A_degree <- A_degree[ind]
-  A_order <- A_order[ind]
-  A_set <- A_set[ind,]
+  A_set <- generate_A(p, md, mo)
+  A_deg <- apply(A_set, 1, sum)
+  A_ord <- apply(A_set, 1, function(aa) sum(aa > 0))
   N_alpha <- nrow(A_set)
 
   phi <- matrix(NA, nrow=n, ncol=N_alpha)
@@ -59,18 +54,25 @@ bayes_chaos_wrapper <- function(X, y, n, p, md, mo, mu_y, sig_y, realmd, lambda,
   }
   ord <- rev(order(rr^2))
   A_set <- A_set[ord,]
+  A_deg <- A_deg[ord]
+  A_ord <- A_ord[ord]
   phi <- phi[,ord]
   rr <- rr[ord]
 
   # Get conditional covariances
   if(verbose) cat("\tComputing partial correlation coefficients\n")
   for(i in 2:N_alpha){
+    if(N_alpha > 50 && ((i %% round(N_alpha/10)) == 0)){
+      if(verbose) cat("\t\t completed ", i, " out of ", N_alpha, ",   ", "\n",sep="")
+    }
     eps_y <- lm(y ~ phi[,1:(i-1)])$residuals
     eps_p <- lm(phi[,i] ~ phi[,1:(i-1)])$residuals
     rr[i] <- cor(eps_y, eps_p)
   }
   ord <- rev(order(rr^2))
   A_set <- A_set[ord,]
+  A_deg <- A_deg[ord]
+  A_ord <- A_ord[ord]
   phi <- phi[,ord]
   rr <- rr[ord]
 
@@ -83,7 +85,7 @@ bayes_chaos_wrapper <- function(X, y, n, p, md, mo, mu_y, sig_y, realmd, lambda,
 
   KIC <- rep(NA, N_alpha+1)
   KIC[1] <- Inf
-  Caa <- diag(c(1, (A_degree + A_order -1)*A_order^2))
+  Caa <- diag(c(1, (A_deg + A_ord -1)*A_ord^2))
   Caa_inv <- diag(1/diag(Caa))
   best <- list(k=0, KIC=Inf, map=NULL)
   for(k in 2:(N_alpha+1)){
@@ -99,7 +101,7 @@ bayes_chaos_wrapper <- function(X, y, n, p, md, mo, mu_y, sig_y, realmd, lambda,
     }
   }
   if(md <= realmd - 2){
-    if(max(A_degree[1:best$k]) + 1 >= md){
+    if(max(A_deg[1:best$k]) + 1 >= md){
       res <- bayes_chaos_wrapper(X, y, n, p, md+2, mo+1, mu_y, sig_y, realmd, lambda, verbose)
       return(res)
     }
@@ -126,6 +128,7 @@ bayes_chaos_wrapper <- function(X, y, n, p, md, mo, mu_y, sig_y, realmd, lambda,
 #' y <- apply(X, 1, duqling::dms_simple) + rnorm(500, 0, 0.1)
 #' fit <- bayes_chaos(X, y)
 #' pred <- predict(fit, X)
+#'
 #' @export
 predict.bayes_chaos <- function(object, newdata=NULL, samples=FALSE){
   if(is.null(newdata)){
@@ -161,6 +164,20 @@ predict.bayes_chaos <- function(object, newdata=NULL, samples=FALSE){
   return(pred)
 }
 
+#' Plot Method for class bayes_chaos
+#'
+#' See \code{bayes_chaos()} for details.
+#'
+#' @param x An object returned by the \code{bayes_chaos()} function.
+#' @param ... additional arguments passed to \code{plot}
+#' @details Plot function for bayes_chaos object.
+#' @references Shao, Q., Younes, A., Fahs, M., & Mara, T. A. (2017). Bayesian sparse polynomial chaos expansion for global sensitivity analysis. Computer Methods in Applied Mechanics and Engineering, 318, 474-496.
+#' @examples
+#' X <- lhs::maximinLHS(500, 3)
+#' y <- apply(X, 1, duqling::dms_simple) + rnorm(500, 0, 0.1)
+#' fit <- bayes_chaos(X, y)
+#' plot(fit)
+#' @export
 plot.bayes_chaos <- function(x, ...){
   pred <- predict(x, x$X, samples=1000)
   yhat <- colMeans(pred)
@@ -173,53 +190,6 @@ plot.bayes_chaos <- function(x, ...){
   }
 }
 
-
-legendre_poly <- function(x, j){
-  if(j == 0){
-    return(rep(1, length(x)))
-  }
-  if(j == 1){
-    return(x)
-  }
-  n <- j - 1
-  res <- ((2*n+1)*x*legendre_poly(x, n) - n*legendre_poly(x, n-1))/(n+1)
-  return(res)
-}
-
-ss_legendre_poly <- function(x, j){
-  sqrt(2*j+1)*legendre_poly(2*x-1, j)
-}
-
-
-
-
-
-estimate_map <- function(y, phi, Cinv, sig0=1, tol=1e-5, max_iter=1000){
-  n <- length(y)
-  p <- ncol(phi)
-  sig_curr <- sig0
-  flag <- TRUE
-  cnt <- 1
-  while(flag){
-    Cinv_curr <-  crossprod(phi)/sig_curr^2 + Cinv
-    C_curr <- solve(Cinv_curr)
-    a_curr <- (tcrossprod(C_curr, phi)%*%y)/sig_curr^2
-    yhat   <- phi%*%a_curr
-    sig_new <- sqrt(mean((y-yhat)^2))
-    delta <- abs(sig_new-sig_curr)
-    sig_curr <- sig_new
-
-    if(delta < tol){
-      flag <- FALSE
-    }
-    if(cnt >= max_iter){
-      flag <- FALSE
-    }
-    cnt <- cnt + 1
-  }
-  out <- list(a=a_curr, sig=sig_curr, Caa_inv=Cinv_curr)
-  return(out)
-}
 
 
 
---
+++ R/chaos_helpers.R
@@ -0,0 +1,72 @@
+legendre_poly <- function(x, j){
+  if(j == 0){
+    return(rep(1, length(x)))
+  }
+  if(j == 1){
+    return(x)
+  }
+  n <- j - 1
+  res <- ((2*n+1)*x*legendre_poly(x, n) - n*legendre_poly(x, n-1))/(n+1)
+  return(res)
+}
+
+ss_legendre_poly <- function(x, j){
+  sqrt(2*j+1)*legendre_poly(2*x-1, j)
+}
+
+generate_A <- function(p, d, q) {
+  # Initialize the result matrix
+  res <- NULL
+
+  # Recursive function
+  generate <- function(current_set, current_sum, current_nonzero, index) {
+    if (current_sum > d || current_nonzero > q) return()
+    if (index > p) {
+      if(current_nonzero > 0){
+        res <<- rbind(res, current_set)
+      }
+      return()
+    }
+
+    for (value in 0:(d - current_sum)) {
+      new_set <- current_set
+      new_set[index] <- value
+      generate(new_set,
+               current_sum + value,
+               current_nonzero + as.numeric(value > 0),
+               index + 1)
+    }
+  }
+
+  # Start the generation process
+  generate(integer(p), 0, 0, 1)
+  return(res)
+}
+
+
+estimate_map <- function(y, phi, Cinv, sig0=1, tol=1e-5, max_iter=1000){
+  n <- length(y)
+  p <- ncol(phi)
+  sig_curr <- sig0
+  flag <- TRUE
+  cnt <- 1
+  while(flag){
+    Cinv_curr <-  crossprod(phi)/sig_curr^2 + Cinv
+    C_curr <- solve(Cinv_curr)
+    a_curr <- (tcrossprod(C_curr, phi)%*%y)/sig_curr^2
+    yhat   <- phi%*%a_curr
+    sig_new <- sqrt(mean((y-yhat)^2))
+    delta <- abs(sig_new-sig_curr)
+    sig_curr <- sig_new
+
+    if(delta < tol){
+      flag <- FALSE
+    }
+    if(cnt >= max_iter){
+      flag <- FALSE
+    }
+    cnt <- cnt + 1
+  }
+  out <- list(a=a_curr, sig=sig_curr, Caa_inv=Cinv_curr)
+  return(out)
+}
